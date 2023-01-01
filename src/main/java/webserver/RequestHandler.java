76e1068deb41e5a5b61857bd8b31da6b01b19c04
khg339 <khg33333339@gmail.com>
2023-01-01 21:59:44 +0900

    feat : Complete 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,9 +3,12 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -23,20 +26,28 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
+            //#1 HTTP 첫 번째 라인을 읽어서 요청 url 값 저장하기
             //BufferedReader을 통해 헤더 값 읽기
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-
             //http header 첫 번째 줄 읽기
             String line = br.readLine();
-
             //null 일 경우 예외처리
             if(line == null) return;
-
             //첫 번째 line 공백으로 나누기
             String[] tokens = line.split(" ");
-
             //두번째 토큰 값이 요청 URL
             String url = tokens[1];
+
+            //#2 회원가입이면 요청 URL 과 이름 값 분리
+            if(url.startsWith("/user/create")){
+                int index = url.indexOf("?");
+                String params = url.substring(index+1);
+                url = url.substring(0, index);
+
+                //#3 불러온 회원 정보를 User 클래스에 저장
+                Map<String, String> userInfo = HttpRequestUtils.parseQueryString(params);
+                User user = new User(userInfo.get("userId"), userInfo.get("password"), userInfo.get("name"), userInfo.get("email"));
+            }
 
             DataOutputStream dos = new DataOutputStream(out);
 
