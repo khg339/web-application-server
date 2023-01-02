894d80d5c3a89a6a261324d2f406ae42e39b6646
khg339 <khg33333339@gmail.com>
2023-01-02 19:29:48 +0900

    feat : Complete 요구사항 3 - POST 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,12 +3,15 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
+import com.sun.javafx.collections.MappingChange;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -37,16 +40,22 @@ public class RequestHandler extends Thread {
             String[] tokens = line.split(" ");
             //두번째 토큰 값이 요청 URL
             String url = tokens[1];
+            //헤더정보 끝까지 읽기
+            Map<String, String> headerInfo = new HashMap<>();
+            while(!"".equals(line = br.readLine())){
+                String[] info = line.split(": ");
+                headerInfo.put(info[0], info[1]);
+            }
 
-            //#2 회원가입이면 요청 URL 과 이름 값 분리
+            //#2 회원가입이면 body 에 저장된 회원정보 읽기
             if(url.startsWith("/user/create")){
-                int index = url.indexOf("?");
-                String params = url.substring(index+1);
-                url = url.substring(0, index);
+                String params = IOUtils.readData(br, Integer.parseInt(headerInfo.get("Content-Length")));
+                log.info("params = " + params);
 
                 //#3 불러온 회원 정보를 User 클래스에 저장
                 Map<String, String> userInfo = HttpRequestUtils.parseQueryString(params);
                 User user = new User(userInfo.get("userId"), userInfo.get("password"), userInfo.get("name"), userInfo.get("email"));
+                log.info("userId = " + user.getUserId() + ", password = " + user.getPassword() + ", name = " + user.getName() + ", email = " + user.getEmail());
             }
 
             DataOutputStream dos = new DataOutputStream(out);
