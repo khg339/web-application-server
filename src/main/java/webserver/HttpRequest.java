b82504b5666ffd3fb843906ceb4ded8e1cfb6aa4
khg339 <khg33333339@gmail.com>
2023-01-14 16:37:46 +0900

    refactor : 요청 데이터 처리 클래스(HttpRequest) 구현

---
+++ src/main/java/webserver/HttpRequest.java
@@ -0,0 +1,103 @@
+package webserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
+import util.IOUtils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
+    private String method;
+    private String path;
+    private Map<String, String> header = new HashMap<>();
+    private Map<String, String> params = new HashMap<>();
+
+    HttpRequest(InputStream inputStream) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            //http header 첫 번째 줄 읽기(요청라인)
+            String line = br.readLine();
+
+            //null 일 경우 예외처리
+            if(line == null) return;
+
+            //요청라인에 따라 GET, POST 처리
+            processRequestLine(line);
+
+            //헤더 정보 저장
+            while(!"".equals(line = br.readLine())){
+                log.info("header : {}", line);
+                String[] info = line.split(": ");
+                header.put(info[0], info[1]);
+            }
+
+            //POST 경우 본문읽기
+            if(method.equals("POST")){
+                String body = IOUtils.readData(br, Integer.parseInt(header.get("Content-Length")));
+                params = HttpRequestUtils.parseQueryString(body);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void processRequestLine(String line){
+        String[] tokens = line.split(" ");
+        method = tokens[0]; //HTTP 메소드 저장
+
+        if("POST".equals(method)){ //POST 방식
+            path = tokens[1];
+            return;
+        }
+
+        //GET 방식
+        int index = tokens[1].indexOf("?");
+        path = tokens[1].substring(0, index); //Path 값
+        params = HttpRequestUtils.parseQueryString(tokens[1].substring(index+1)); //쿼리스트링 값
+    }
+
+    public String getHeader(String field){
+        return header.get(field);
+    }
+
+    public String getParameter(String field){
+        return params.get(field);
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public Map<String, String> getHeader() {
+        return header;
+    }
+
+    public void setHeader(Map<String, String> header) {
+        this.header = header;
+    }
+
+    public Map<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(Map<String, String> params) {
+        this.params = params;
+    }
+}
