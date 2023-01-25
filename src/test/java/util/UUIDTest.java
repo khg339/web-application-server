5c374acf393620e404fc33469e3cec00b8566052
khg339 <khg33333339@gmail.com>
2023-01-25 22:42:09 +0900

    test : UUID 생성 Test

---
+++ src/test/java/util/UUIDTest.java
@@ -0,0 +1,12 @@
+package util;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+public class UUIDTest {
+    @Test
+    public void uuid(){
+        System.out.println(UUID.randomUUID());
+    }
+}
